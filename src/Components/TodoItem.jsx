a2bd2ea466853fedf8fe6a308d3ff230905f7816
John Navarro <alessandro7833@gmail.com>
2023-02-05 15:34:00 -0600

    Checkbox function finished

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -1,13 +1,32 @@
 import React from "react";
+import '../App.css';
 
 class TodoItem extends React.Component {
+constructor(){
+    super();
+    this.state = {checked: false}
+    this.handleToggleClick = this.handleToggleClick.bind(this)
+}
+
+handleToggleClick(){
+    this.setState(prevState => ({
+        checked: !prevState.checked
+    }));
+}
+
     render() {
         return(
-            <li>
-                {this.props.text}
-            </li>
+            <div className="todoList">
+                <input
+                    onChange={this.handleToggleClick}
+                    type={"checkbox"}>
+                </input>
+                {this.state.checked ? <li className="todoStyleChecked">{this.props.text}</li> : <li className="todoStyle"> {this.props.text} </li> }
+            </div>
         );
     }
+
+   
 }
 
 export default TodoItem;
