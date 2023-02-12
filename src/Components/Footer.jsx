cc2538b5ac4ea1cadd005621788d8c2a53125cd0
John Navarro <alessandro7833@gmail.com>
2023-02-12 14:36:31 -0600

    Footer component finished

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import '../App.css';
+
+class Footer extends React.Component {
+
+    render(){
+        return(
+            <div className="footerStyle">
+                <a href="https://github.com/Jonnas098">
+                    Created by @Jonnas098 | 2023
+                </a>
+            </div>
+        )
+    }
+}
+
+export default Footer
